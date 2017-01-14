f36c3286b5fc9d4d5280bd065d1a25b803ddf3b8
Endre Simo <esimovaq@yahoo.com>
2017-01-14 14:37:20 +0200

    Cleanup & add white background

--- drawer.go
+++ drawer.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"text/template"
 	"io"
+	"image/draw"
 )
 
 type ImageDrawer interface {
@@ -70,7 +71,7 @@ func (svg *SVG) Draw(output io.Writer, points [][]float64, debug bool) {
 	oldY := spline.Interpolate(0.0, 0.5)[1]
 	pointStart := Pt(oldX, oldY)
 
-	for t := 0.0; t <= 1.0; t += 0.002 {
+	for t := 0.0; t <= 1.0; t += 0.001 {
 		interpol = spline.Interpolate(t, 0.5)
 		x := interpol[0]
 		y := interpol[1]
@@ -102,10 +103,14 @@ func (img *Image) Draw(output io.Writer, points [][]float64, debug bool) {
 	var interpol []float64
 	var raster *Canvas
 
+	// Draw white background
+	background := color.RGBA{255, 255, 255, 255}
+	draw.Draw(canvas.Image, canvas.Image.Bounds(), &image.Uniform{background}, image.ZP, draw.Src)
+
 	// Draw original line
 	if debug {
 		for i:=0; i < len(points)-1; i++ {
-			raster = canvas.DrawLine(points[i][0], points[i][1], points[i+1][0], points[i+1][1], color.NRGBA{R:155,G:155,B:155,A:155}, false)
+			raster = canvas.DrawLine(points[i][0], points[i][1], points[i+1][0], points[i+1][1], color.NRGBA{R:155,G:155,B:155,A:70 }, false)
 		}
 	}
 
